dd295e0ee60702885a7656f24c1b5ab911730fd7
Joseph Adams <joseph.adams@blueyonder.co.uk>
2021-05-17 06:47:17 +0100

    After connection pooling - removed the conn

--- server/database/connection-pool.js
+++ server/database/connection-pool.js
@@ -1,6 +1,18 @@
+'use strict';
+
+const mysql = require('mysql');
+const db = require('../config/db');
+
 const connectionPool = {
+
+    pool: null,
+    
     init: function() {
-        console.log('init pool');
+        this.pool = mysql.createPool(db);
+    },
+
+    getPool: function() {
+        return this.pool;
     }
 };
 
