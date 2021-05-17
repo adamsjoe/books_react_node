c7dc82b61a749686a034d5ae588b3e80ef184d39
Joseph Adams <joseph.adams@blueyonder.co.uk>
2021-05-17 06:35:06 +0100

    Pre pooling

---
+++ client/src/Book.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Book.css';
+
+function Book() {
+    return(
+        <div id='book-message'>This is a book!</div>
+    );
+}
+
+export default Book;
---
+++ server/routes/books.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql');
+const router = express.Router();
+
+const db = require('../config/db');
+
+console.log(db.user, db.password);
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+  // create a connection
+  const conn = mysql.createConnection(db);
+  
+  // connect to the db, err is a callback to check if connection success
+  conn.connect(err => {
+    if(err) throw err;
+
+    // create a book object
+    const book = {
+      'author':'Charles Dickens',
+      'title':'Great Expectations',
+      'published':'1861-01-01'
+    };
+
+    // run a query to insert
+    conn.query('insert into books set ?', book, (err, result) => {
+      if(err) throw err;
+
+      console.log(result);
+    });
+  });
+  res.send('books here!');
+});
+
+module.exports = router;
